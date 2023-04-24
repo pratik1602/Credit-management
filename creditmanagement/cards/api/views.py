84a1d26e0aa0c1dbfc9cde7a5d7078c9808cd681
Pratik Scalelot <scalelotp@gmail.com>
2023-04-24 17:44:36 +0530

    update commit 24-04-2023

--- creditmanagement/cards/api/views.py
+++ creditmanagement/cards/api/views.py
@@ -60,105 +60,106 @@ class UserCardAPIView(APIView):
     def get(self, request):
         token = get_object(request) 
         if token:
-            get_user = User.objects.get(id = token["user_id"])
-            if get_user and get_user.is_verified:
-                card_id = request.GET.get("card_id")
-                if card_id != None or 0:
-                    try:
-                        card_obj = Card.objects.get(card_id=card_id, user_id=get_user)
-                        serializer = UserCardSerializer(card_obj)
-                        return onSuccess("Your Card !!!", serializer.data)
-                    except:
-                        return badRequest( "No card found !!!")
-                else:
-                    card_objs = Card.objects.filter(user_id=get_user)
-                    if card_objs:
-                        serializer = UserCardSerializer(card_objs, many=True)
-                        return onSuccess("Your Cards !!!", serializer.data)
-                    else:
-                        serializer = UserCardSerializer(card_objs, many=True)
-                        return onSuccess("Your Cards !!!", serializer.data)
-            else:
+            try:
+                get_user = User.objects.get(id = token["user_id"], is_verified = True)
+            except:
                 return badRequest("No User Found or not verified !!!")
+            card_id = request.GET.get("card_id")
+            if card_id != None or 0:
+                try:
+                    card_obj = Card.objects.get(card_id=card_id, user_id=get_user)
+                    serializer = UserCardSerializer(card_obj)
+                    return onSuccess("Your Card !!!", serializer.data)
+                except:
+                    return badRequest( "No card found !!!")
+            else:
+                card_objs = Card.objects.filter(user_id=get_user)
+                if card_objs:
+                    serializer = UserCardSerializer(card_objs, many=True)
+                    return onSuccess("Your Cards !!!", serializer.data)
+                else:
+                    serializer = UserCardSerializer(card_objs, many=True)
+                    return onSuccess("Your Cards !!!", serializer.data)
+                
         else:
             return unauthorisedRequest()
 
     def post(self, request): 
         token = get_object(request)
         if token:
-            get_user = User.objects.get(id= token["user_id"])
-            if get_user and get_user.is_verified:
-                data = request.data
-                if data["card_bank_name"] != "" and data["card_category"] != "" and data["card_number"] != "" and data["card_network"] != "" and data["card_holder_name"] != "" and data["card_photo"] != "" and data["card_exp_date"] != "" and  data["card_cvv"] != "" and data["due_date"] != "" and data["due_amount"] != "":
-                    user_card_number = Card.objects.filter(card_number = data["card_number"])
-                    if not user_card_number:
-                        data["created_by"] = get_user.id
-                        data["updated_by"] = get_user.id
-                        data["due_amount"] = "%.2f" % float(data["due_amount"])
-                        serializer = UserCardSerializer(data=data)
-                        if serializer.is_valid():
-                            serializer.save()
-                            getCard = Card.objects.get(card_id=serializer.data["card_id"])
-                            
-                            getCard.profit_amount =  getCard.due_amount * getCard.commission/100
-                            getCard.profit_amount = "%.2f" % float(getCard.profit_amount)
-                            getCard.user_id = get_user
-                            getCard.created_by = get_user
-                            getCard.updated_by = get_user
-                            getCard.save()
-                            return onSuccess("Card Added Successfully", serializer.data)
-                        else:
-                            return badRequest("Something went wrong !!!")
-                    else:
-                        return badRequest("Invalid card number or already exists !!!")
-                else:
-                    return badRequest("Fields is missing !!!")
-            else:
+            try:
+                get_user = User.objects.get(id = token["user_id"], is_verified = True)
+            except:
                 return badRequest("User not found or not verified !!!")
+            data = request.data
+            if data["card_bank_name"] != "" and data["card_category"] != "" and data["card_number"] != "" and data["card_network"] != "" and data["card_holder_name"] != "" and data["card_photo"] != "" and data["card_exp_date"] != "" and  data["card_cvv"] != "" and data["due_date"] != "" and data["due_amount"] != "":
+                user_card_number = Card.objects.filter(card_number = data["card_number"])
+                if not user_card_number:
+                    data["created_by"] = get_user.id
+                    data["updated_by"] = get_user.id
+                    data["due_amount"] = "%.2f" % float(data["due_amount"])
+                    serializer = UserCardSerializer(data=data)
+                    if serializer.is_valid():
+                        serializer.save()
+                        getCard = Card.objects.get(card_id=serializer.data["card_id"])
+                        
+                        getCard.profit_amount =  getCard.due_amount * getCard.commission/100
+                        getCard.profit_amount = "%.2f" % float(getCard.profit_amount)
+                        getCard.user_id = get_user
+                        getCard.created_by = get_user
+                        getCard.updated_by = get_user
+                        getCard.save()
+                        return onSuccess("Card Added Successfully", serializer.data)
+                    else:
+                        return badRequest("Something went wrong !!!")
+                else:
+                    return badRequest("Invalid card number or already exists !!!")
+            else:
+                return badRequest("Fields is missing !!!")  
         else:
             return unauthorisedRequest()
 
     def patch(self, request):
         token = get_object(request)
         if token:
-            get_user = User.objects.get(id = token["user_id"])
-            if get_user and get_user.is_verified:
-                try:
-                    data = request.data
-                    get_card = Card.objects.get(card_id = data["card_id"])  
-                    data["due_amount"] = "%.2f" % float(data["due_amount"])
-                    serializer = UserCardSerializer(get_card ,data=data, partial = True)
-                    if serializer.is_valid():
-                        serializer.save()
-                        get_card.profit_amount =  get_card.due_amount * get_card.commission / 100
-                        get_card.profit_amount = "%.2f" % float(get_card.profit_amount)
-                        get_card.updated_by = get_user
-                        get_card.modified_at = datetime.now()
-                        get_card.save()
-                        return onSuccess("Card updated Successfully !!!", serializer.data)
-                    else:
-                        return badRequest("Something went wrong !!!")
-                except:
-                    return badRequest("Card doesn't exixts !!!")
+            try:
+                get_user = User.objects.get(id = token["user_id"], is_verified = True)
+            except:
+                return badRequest("User not found or not verified !!!")
+            data = request.data
+            try:
+                get_card = Card.objects.get(card_id = data["card_id"])  
+            except:
+                return badRequest("Card doesn't exixts !!!")
+            data["due_amount"] = "%.2f" % float(data["due_amount"])
+            serializer = UserCardSerializer(get_card ,data=data, partial = True)
+            if serializer.is_valid():
+                serializer.save()
+                get_card.profit_amount =  get_card.due_amount * get_card.commission / 100
+                get_card.profit_amount = "%.2f" % float(get_card.profit_amount)
+                get_card.updated_by = get_user
+                get_card.modified_at = datetime.now()
+                get_card.save()
+                return onSuccess("Card updated Successfully !!!", serializer.data)
             else:
-                return badRequest("User not found !!!")
+                return badRequest("Something went wrong !!!")
         else:
             return unauthorisedRequest()
 
     def delete(self, request):
         token = get_object(request)
         if token:
-            get_user = User.objects.get(id = token["user_id"])
-            if get_user and get_user.is_verified:
-                card_id = request.GET.get("card_id")
-                try:
-                    card_obj = Card.objects.get(card_id=card_id, user_id=get_user)
-                    card_obj.delete()
-                    return onSuccess("Card deleted successfully !!!", 1)
-                except:
-                    return badRequest("Card doesn't exists")
-            else:
-                return badRequest("No user found !!!")
+            try:
+                get_user = User.objects.get(id = token["user_id"], is_verified = True)
+            except:
+                return badRequest("User not found or not verified !!!") 
+            card_id = request.GET.get("card_id")
+            try:
+                card_obj = Card.objects.get(card_id=card_id, user_id=get_user)
+                card_obj.delete()
+                return onSuccess("Card deleted successfully !!!", 1)
+            except:
+                return badRequest("Card doesn't exists")
         else:
             return unauthorisedRequest()
 
@@ -169,92 +170,94 @@ class AdminCards(APIView):
     def get(self, request):
         token = get_object(request)
         if token:
-            get_admin = User.objects.get(id = token["user_id"])
-            if get_admin and get_admin.is_admin:
-                card_id = request.GET.get("card_id")
-                card_type = request.GET.get("card_type")
-                if card_id != None or 0:
-                    try:
-                        card_obj = Card.objects.get(card_id=card_id, user_id=get_admin)
-                        serializer = AdminAllcardSerializer(card_obj)
-                        return onSuccess("Your Card !!!", serializer.data)
-                    except:
-                        return badRequest("No card found !!!")  
-                else:
-                    if card_type:
-                        card_objs = Card.objects.filter(user_id=get_admin, card_type = card_type)
-                    else:
-                        card_objs = Card.objects.filter(user_id=get_admin)
- 
-                    serializer = AdminAllcardSerializer(card_objs, many=True)
-                    return onSuccess("Your Cards !!!", serializer.data)  
-            else:
+            try:
+                get_admin = User.objects.get(id = token["user_id"], is_admin = True)
+            except:
                 return badRequest("No Admin Found !!!")
+            card_id = request.GET.get("card_id")
+            card_type = request.GET.get("card_type")
+            if card_id != None or 0:
+                try:
+                    card_obj = Card.objects.get(card_id=card_id, user_id=get_admin)
+                    serializer = AdminAllcardSerializer(card_obj)
+                    return onSuccess("Your Card !!!", serializer.data)
+                except:
+                    return badRequest("No card found !!!")  
+            else:
+                if card_type:
+                    card_objs = Card.objects.filter(user_id=get_admin, card_type = card_type)
+                else:
+                    card_objs = Card.objects.filter(user_id=get_admin)
+
+                serializer = AdminAllcardSerializer(card_objs, many=True)
+                return onSuccess("Your Cards !!!", serializer.data)          
         else:
             return unauthorisedRequest()
 
     def post(self, request):
         token = get_object(request)
         if token:
-            get_admin = User.objects.get(id = token["user_id"])
-            if get_admin and get_admin.is_admin:
-                data = request.data
-                if data["card_bank_name"] != "" and data["card_category"] != "" and data["card_number"] != "" and data["card_network"] != "" and data["card_holder_name"] != "" and data["card_photo"] != "" and data["card_exp_date"] != "" and  data["card_cvv"] != ""  and data["credit_amount"] != "":
-                    user_card_number = Card.objects.filter(card_number = data["card_number"])
-                    if not user_card_number:   
-                        data["user_id"] = get_admin.id
-                        data["credit_amount"] = "%.2f" % float(data["credit_amount"])
-                        serializer = AdminCreditCardSerializer(data=data)
-                        if serializer.is_valid():
-                            serializer.save()
-                            return onSuccess("Credit card added successfully !!!", serializer.data)
-                        else:
-                            return badRequest(serializer.errors)
-                    else:
-                        return badRequest("Card with this card number already exists !!!")
-                else:
-                    return badRequest("Fields is missing !!!")
-            else:
+            try:
+                get_admin = User.objects.get(id = token["user_id"], is_admin = True)
+            except:
                 return badRequest("Admin not found !!!")
+            data = request.data
+            if data["card_bank_name"] != "" and data["card_category"] != "" and data["card_number"] != "" and data["card_network"] != "" and data["card_holder_name"] != "" and data["card_photo"] != "" and data["card_exp_date"] != "" and  data["card_cvv"] != ""  and data["credit_amount"] != "":
+                user_card_number = Card.objects.filter(card_number = data["card_number"])
+                if not user_card_number:   
+                    data["user_id"] = get_admin.id
+                    data["credit_amount"] = "%.2f" % float(data["credit_amount"])
+                    serializer = AdminCreditCardSerializer(data=data)
+                    if serializer.is_valid():
+                        serializer.save()
+                        return onSuccess("Credit card added successfully !!!", serializer.data)
+                    else:
+                        return badRequest(serializer.errors)
+                else:
+                    return badRequest("Card with this card number already exists !!!")
+            else:
+                return badRequest("Fields is missing !!!")               
         else:
             return unauthorisedRequest()
             
     def patch(self, request):
+        if not request.POST._mutable:
+            request.POST._mutable = True
         token = get_object(request)
         if token:
-            get_admin = User.objects.get(id = token["user_id"])
-            if get_admin and get_admin.is_admin:
-                data = request.data
-                try:
-                    get_card = Card.objects.get(card_id = data["card_id"])
-                except:
-                    return badRequest("Card doesn't exixts !!!")
-                data["credit_amount"] = "%.2f" % float(data["credit_amount"])
-                serializer = AdminCreditCardSerializer(get_card ,data=data, partial = True)
-                if serializer.is_valid():
-                    serializer.save()
-                    return onSuccess("Card updated Successfully !!!", serializer.data)
-                else:
-                    return badRequest("Something went wrong !!! ")     
-            else:
+            try:
+                User.objects.get(id = token["user_id"], is_admin = True)
+            except:
                 return badRequest("Admin not found !!!")
+            data = request.data
+            try:
+                get_card = Card.objects.get(card_id = data["card_id"])
+            except:
+                return badRequest("Card doesn't exixts !!!")
+            data["credit_amount"] = "%.2f" % float(data["credit_amount"])
+            serializer = AdminCreditCardSerializer(get_card ,data=data, partial = True)
+            if serializer.is_valid():
+                serializer.save()
+                return onSuccess("Card updated Successfully !!!", serializer.data)
+            else:
+                return badRequest("Something went wrong !!! ") 
         else:
             return unauthorisedRequest() 
            
     def delete(self, request):
         token = get_object(request)
         if token:
-            get_admin = User.objects.get(id = token["user_id"])
-            if get_admin and get_admin.is_admin:
-                card_id = request.GET.get("card_id")
-                try:
-                    card_obj = Card.objects.get(card_id=card_id, user_id=get_admin)
-                    card_obj.delete()
-                    return onSuccess("Card deleted successfully !!!")
-                except:
-                    return badRequest("Card doesn't exists")
-            else:
-                return badRequest("No user found !!!")
+            try:
+                get_admin = User.objects.get(id = token["user_id"], is_admin = True)
+            except:
+                return badRequest("Admin not found !!!")
+            card_id = request.GET.get("card_id")
+            try:
+                card_obj = Card.objects.get(card_id=card_id, user_id=get_admin)
+                card_obj.delete()
+                return onSuccess("Card deleted successfully !!!")
+            except:
+                return badRequest("Card doesn't exists")
         else:
             return unauthorisedRequest()
 
